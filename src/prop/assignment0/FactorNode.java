29258571feffc832cbd7d32b1af748080660356f
Freezaa9 <geoffreyharrazi@gmail.com>
2016-11-17 21:57:45 +0100

    Add class node

---
+++ src/prop/assignment0/FactorNode.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prop.assignment0;
+
+/**
+ *
+ * @author Geoffrey-Port
+ */
+public class FactorNode implements INode {
+    	private char ident;
+	private char assignOp;
+        private ExpressionNode expr;
+
+	public FactorNode() {
+
+		//System.out.println("Lexeme:" + value + " " + token);
+	}
+    @Override
+    public Object evaluate(Object[] args) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buildString(StringBuilder builder, int tabs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/prop/assignment0/Parser.java
+++ src/prop/assignment0/Parser.java
@@ -18,7 +18,8 @@ class Parser implements IParser{
 	    if (t == null)
 		throw new IOException("No open file.");
 
-	    return new TextNode(t);	    
+	    return null;//new TextNode(t);	
+            
 	}
 
     @Override
@@ -27,135 +28,8 @@ class Parser implements IParser{
 	    t.close();
     }
     
-    class TextNode implements INode{
-	SentenceNode s = null;
-	TextNode tx = null;
-
-	public TextNode(Tokenizer t) {
-	    s = new SentenceNode(t);
-	    if (t.current().token() != Token.EOF)
-		tx = new TextNode(t);
-	}
-
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
-    class SentenceNode implements INode{
-	NounphraseNode n = null;
-	VerbphraseNode v = null;
-
-	public SentenceNode(Tokenizer t) {
-	    n = new NounphraseNode(t);
-	    v = new VerbphraseNode(t);
-	}
-
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
-    class NounphraseNode implements INode {
-	DeterminerNode d = null;
-	NounNode n = null;
-	public NounphraseNode(Tokenizer t) {
-	    d = new DeterminerNode(t);
-	    n = new NounNode(t);
-	}
-
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
-    class VerbphraseNode implements INode {
-	VerbNode v = null;
-	NounphraseNode n = null;
-
-	public VerbphraseNode(Tokenizer t) {
-	    v = new VerbNode(t);
-	    n = new NounphraseNode(t);
-	}
-
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
-    class DeterminerNode implements INode {
-	Lexeme l = null;
-	public DeterminerNode(Tokenizer t) {
-	    // get next token and check that it's a determiner
-	}
-
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
-    class NounNode implements INode {
-	Lexeme l = null;
-	public NounNode(Tokenizer t) {
-	    // get next token and check that it's a Noun
-	}
-
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
-    class VerbNode implements INode {
-	Lexeme l = null;
-	public VerbNode(Tokenizer t) {
-	    // get next token and check that it's a Verb
-	}
-
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-
+    
+    
+    
+    
 }
